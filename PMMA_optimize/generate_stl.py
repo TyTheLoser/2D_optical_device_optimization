27bff7657a226ff06e57c4ae617bf10eb1ded6d8
landinGuy <ty13070501155@163.com>
2025-09-25 18:06:37 +0800

    参数修改

--- PMMA_optimize/generate_stl.py
+++ PMMA_optimize/generate_stl.py
@@ -1,9 +1,9 @@
 from utils.utils_ray_trace import generate_device_stl_from_npz
 # --- 2. 调用核心函数 ---
-output_stl_file = 'PMMA_optimize/output/bottom_3_lights_116_7_0.stl'
-demo_npz_file = 'PMMA_optimize/output/bottom_3_lights_116_7_0.npz'
-device_x_range = (0, 8.3)
-device_y_range = (0, 1) # 定义器件的宽度
+output_stl_file = 'PMMA_optimize/output/bottom_3_lights_116_7_BEST.stl'
+demo_npz_file = 'PMMA_optimize/output/bottom_3_lights_116_7_BEST.npz'
+device_x_range = (0, 16)
+device_y_range = (0, 10) # 定义器件的宽度
 
 # 运行生成器
 light_positions = generate_device_stl_from_npz(
